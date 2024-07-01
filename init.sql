a6101f98ec1636f2313074d066da07fc0d486d03
Xi Xu <i@xi-xu.me>
2024-07-02 01:29:40 +0800

    Create init.sql

---
+++ init.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE IF NOT EXISTS lda;
+
+USE lda;
+
+CREATE TABLE IF NOT EXISTS
+    user(
+        userName VARCHAR(255) NOT NULL PRIMARY KEY,
+        password VARCHAR(255) NOT NULL,
+        name VARCHAR(255),
+        role VARCHAR(255)
+    );
+
+CREATE TABLE IF NOT EXISTS
+    book (
+        title VARCHAR(255) NOT NULL PRIMARY KEY,
+        author VARCHAR(255) NOT NULL,
+        publisher VARCHAR(255),
+        oldprice DECIMAL(10, 2),
+        newprice DECIMAL(10, 2),
+        href VARCHAR(255),
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+
+INSERT INTO
+    user
+VALUES
+    ('admin', 'admin', 'admin', 'admin');
